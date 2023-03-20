aa8827fce7014f9504dcc7b9b16b5cfbef722892
shibarin <97890981+sbmtrntr@users.noreply.github.com>
2023-03-20 22:51:46 +0900

    2023/03/20

---
+++ ABC/285/B.py
@@ -0,0 +1,9 @@
+S = input()
+
+def alpha2num(alpha):
+    num=0
+    for index, item in enumerate(list(alpha)):
+        num += pow(26,len(alpha)-index-1)*(ord(item)-ord('A')+1)
+    return num
+
+print(alpha2num(S))
