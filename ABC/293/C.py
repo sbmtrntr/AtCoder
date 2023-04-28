0be4278e703793302c5b8e0afd0d4cb54c522bf0
shibarin <shibarin317@icloud.com>
2023-04-28 23:26:20 +0900

    2023/04/28

--- ABC/293/C.py
+++ ABC/293/C.py
@@ -1,22 +1,29 @@
-from collections import deque
 H, W = map(int, input().split())
-A = [
-    list(map(int, input().split())) for _ in range(H)
-]
+A = [list(map(int, input().split())) for _ in range(H)]
+ans = 0
+for b in range((1 << H+W-2)):
+    temp = ""
+    seen = set()
+    seen.add(A[0][0])
+    for i in range(H+W-2):
+        if b >> i & 1:
+            temp += "1"
+        else:
+            temp += "0"
+    cnt = 0
+    for s in temp:
+        if s == "0":
+            cnt += 1
 
-sx = 0
-sy = 0
-Q = deque()
-Q.append(sx, sy)
-dist = [[-1]*W for _ in range(H)]
-dist[0][0] = 0
-dirc = [(1, 0), (0, 1)]
+    if cnt == H-1:
+        x, y = 0, 0
+        for s in temp:
+            if s == "0":
+                x += 1
+            else:
+                y += 1    
+            seen.add(A[x][y])
+        if len(seen) == H+W-1:
+            ans += 1
 
-while len(Q) > 0:
-    y, x = Q.popleft()
-    for dy, dx in dirc:
-        y2 = y + dy
-        x2 = x + dx
-
-        if not (0 <= y2 < H and 0 <= x2 < W):
-            continue
+print(ans)
