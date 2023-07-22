de5276f6e06c439da9de34191ceccb77b6205a19
shibarin <shibarin317@icloud.com>
2023-07-22 16:07:54 +0900

    2023/07/22

---
+++ template_method/BFS.py
@@ -0,0 +1,11 @@
+def bfs(u, hoge):
+    queue = deque([u])
+    d = [-1] * (hoge+1) # uからの距離の初期化
+    d[u] = 0 # 自分との距離は0
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if d[i] == -1:
+                d[i] = d[v] + 1
+                queue.append(i)
+    return d
