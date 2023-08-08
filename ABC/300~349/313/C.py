6de57423d6d0884806759e56055c6dae8e26a7e7
shibarin <shibarin317@icloud.com>
2023-08-08 20:27:12 +0900

    2023/08/08

--- ABC/300~349/313/C.py
+++ ABC/300~349/313/C.py
@@ -1,28 +1,13 @@
 N = int(input())
 A = list(map(int, input().split()))
-mean = sum(A) // N
 A.sort()
-l = 0
-r = N-1
 ans = 0
-while l < r:
-    if abs(A[l] - mean) > abs(A[r] - mean):
-        ans += abs(A[r] - mean)
-        A[l] += abs(A[r] - mean)
-        A[r] -= abs(A[r] - mean)
-        r -= 1
-        if abs(A[l] - mean) <= 1:
-            l += 1
-    else:
-        ans += abs(A[l] - mean)
-        A[r] -= abs(A[l] - mean)
-        A[l] += abs(A[l] - mean)
-        l += 1
-        if abs(A[r] - mean) <= 1:
-            r -= 1
+p = sum(A) // N
+r = sum(A) % N
 
-for i in range(N):
-    if abs(A[i] - mean) > 1:
-        ans += abs(A[i] - mean) - 1
+for i in range(N-r):
+    ans += abs(A[i] - p)
+for i in range(N-r, N):
+    ans += abs(A[i] - (p + 1))
 
-print(ans)
+print(ans//2)
