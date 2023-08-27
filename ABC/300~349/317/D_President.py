b3171151b68749336be359f06cd2bf544aea9887
shibarin <shibarin317@icloud.com>
2023-08-27 11:14:52 +0900

    2023/08/27

--- ABC/300~349/317/D_President.py
+++ ABC/300~349/317/D_President.py
@@ -7,12 +7,19 @@ victory = (sum(Z)+1) // 2
 major = [0]*N
 
 for i in range(N):
-    major[i] = max(0, (Y[i] - X[i] + 1)//2)
+    major[i] = max(0, Y[i] - (X[i] + Y[i])//2)
 
-INF = 10**10
-S = sum(Z)
-dp = [INF]*(S+1)
+INF = 10**12
+S = sum(Z)+1
+dp = [[INF]*(S) for _ in range(N+1)]
+
+dp[0][0] = 0
 
 for i in range(N):
     for j in range(S):
-        dp[j] = min(dp[j], dp[j-major[i]]+)
+        if dp[i][j] != INF and j+Z[i] <= S:
+                dp[i+1][j+Z[i]] = min(dp[i][j+Z[i]], dp[i][j] + major[i])
+
+        dp[i+1][j] = min(dp[i+1][j], dp[i][j])
+
+print(min(dp[N][victory:]))
