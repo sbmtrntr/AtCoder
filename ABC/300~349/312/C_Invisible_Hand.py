ac92e1cba038fa5f30aaa1a7822453ac7d2e1d04
shibarin <shibarin317@icloud.com>
2023-09-15 13:45:27 +0900

    2023/09/15

--- ABC/300~349/312/C_Invisible_Hand.py
+++ ABC/300~349/312/C_Invisible_Hand.py
@@ -1,15 +1,24 @@
-from bisect import bisect_left
+from bisect import bisect_right, bisect_left
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 A.sort()
 B.sort()
 
-for i in range(N):
-    idx = bisect_left(B, A[i])
-    if idx == M:
-        continue
-    if i+1 >= M - idx:
-        print(A[i])
-        exit()
-print(B[-1]+1)
+def f(x):
+    return bisect_right(A, x)
+
+def g(x):
+    return M - bisect_left(B, x)
+
+left = 0
+right = 10**9
+
+while right - left > 1:
+    mid = (right + left) // 2
+    if f(mid) >= g(mid):
+        right = mid
+    else:
+        left = mid
+
+print(right)
